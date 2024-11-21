3ff0915d83795dc99e8290bd376d72d11783cee3
Krabinostav <84402474+Krabinostav@users.noreply.github.com>
2024-11-21 18:32:15 +0100

    Initial Push

---
+++ GameEngine/GameMaster/gameMaster.py
@@ -0,0 +1,83 @@
+import time
+from datetime import datetime
+import random
+import paho
+from paho.mqtt import client as mqtt_client
+import os
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+    # For paho-mqtt 2.0.0, you need to add the properties parameter.
+    # def on_connect(client, userdata, flags, rc, properties):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id=client_id)
+
+    # For paho-mqtt 2.0.0, you need to set callback_api_version.
+    # client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
+
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+FIRST_RECONNECT_DELAY = 1
+RECONNECT_RATE = 2
+MAX_RECONNECT_COUNT = 12
+MAX_RECONNECT_DELAY = 60
+
+def on_disconnect(client, userdata, rc):
+    print("Disconnected with result code: %s", rc)
+    reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
+    while reconnect_count < MAX_RECONNECT_COUNT:
+        print("Reconnecting in %d seconds...", reconnect_delay)
+        time.sleep(reconnect_delay)
+
+        try:
+            client.reconnect()
+            print("Reconnected successfully!")
+            return
+        except Exception as err:
+            print("%s. Reconnect failed. Retrying...", err)
+
+        reconnect_delay *= RECONNECT_RATE
+        reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
+        reconnect_count += 1
+    print("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+    client.loop_stop()
+
+def publish(client, msg, topic):
+    result = client.publish(topic, msg)
+    # result: [0, 1]
+    status = result[0]
+    if status == 0:
+        print(f"Send `{msg}` to topic `{topic}`")
+    else:
+        print(f"Failed to send message to topic {topic}")
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        publish(client, "The Meeple Moved", "Board/Output")
+
+    client.subscribe("Meeple")
+    client.on_message = on_message
+
+client_id = ""
+if os.getenv('MQTT_Host') == None:
+    broker = "localhost"
+else :
+    broker = os.getenv('MQTT_Host')
+
+port = 1883
+
+if __name__ == '__main__':
+    print("Host : ", broker)
+    print("Port : ", port)
+    client = connect_mqtt()
+    subscribe(client)
+
+    client.loop_forever()
